f11ee654c143c7ac01284240991482b474640c9d
vit-oucher <vitaliy.oucher.2@gmail.com>
2022-07-29 14:08:55 +0300

    tasks1-5

---
+++ HW3.js
@@ -0,0 +1,149 @@
+// ============== Task1 ====================
+
+// var myName = 'Vitalii';
+// let myAge = +26;
+// const isWork = true;
+// let taskMark = null;
+// let id = Symbol("id")
+// let bigIntNum = 256n;
+// const myObj = {
+//     courses: 'front-end',
+//     lessonName:'js1',
+//     totalNumOfLessons: +5,
+// }
+// console.log(bigIntNum)
+// console.log(taskMark);
+// console.log(myName);
+// console.log(isWork);
+// console.log(id);
+// console.log(myAge);
+// console.log(myObj);
+
+//======================== Task2 =====================
+
+// var myName = 'Vitalii';
+// let myAge = +26;
+// const nationality = 'UKR';
+//
+// myName = 'Vitalii Ocheretnyi';
+// myAge = +27;
+//
+// console.log(myName);
+// console.log(myAge);
+// console.log(nationality);
+
+//====================== Task3 =======================
+// let a = 15;
+// let b = '92';
+// let c;
+//
+// // a = '85';
+// // b = 5
+// // a = 'Andrii '
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+//
+//
+// // console.log(c = +a + b);
+// console.log(c = a + + b);
+// console.log(c > a);
+// console.log(c > b);
+// console.log(c <= b/a);
+//
+// console.log(typeof c);
+//
+// let d = String(a);
+// console.log(d);
+// console.log(typeof d);
+//
+// d = a.toString();
+// console.log(d);
+// console.log(typeof d);
+//
+// d = parseInt(b);
+// console.log(d);
+// console.log(typeof d);
+//
+// d = parseFloat(c);
+// console.log(c);
+// console.log(typeof c);
+
+// // =================================== Task4 ===========================
+// let a = ' Vitalii ';
+// let b = +26;
+// const c = true;
+// let d = null;
+// let e = Symbol("id")
+// let f = 256n;
+// const g = {
+//     courses: 'front-end',
+//     lessonName:'js1',
+//     totalNumOfLessons: +5,
+// }
+// console.log(y = a + b);
+// console.log(y = + b + a);
+// console.log(y = + b + + a);
+// console.log(y = a + c);
+// console.log(y = a + + c);
+// console.log(y = a + d);
+// console.log(y = a + + d);
+// // console.log(y = a + e); - doesn`t work
+// // console.log(y = a + + e);  - doesn`t work
+// console.log(y = a + f);
+// console.log(y = a + g);
+// console.log(y = a + + g);
+
+
+// console.log(y = + b + a);
+// console.log(y = + b + + a);
+// console.log(y = b + c);
+// console.log(y = b + + c);
+// console.log(y = b + d);
+// console.log(y = b + + d);
+// // console.log(y = b + e); - doesn`t work
+// // console.log(y = b + + e); - doesn`t work
+// // console.log(y = b + + f);  - doesn`t work
+// console.log(y = b + g);
+// console.log(y = b + + g);
+
+// =================================== Task5 ===========================
+// let a = ' Vitalii ';
+// let b = +26;
+// const c = true;
+// let d = null;
+// let e = Symbol("id")
+// let f = 256n;
+// const g = {
+//     courses: 'front-end',
+//     lessonName:'js1',
+//     totalNumOfLessons: +5,
+// }
+
+// console.log(y = a - b);
+// console.log(y = + b - a);
+// console.log(y = + b - + a);
+// console.log(y = a - c);
+// console.log(y = a - + c);
+// console.log(y = a - d);
+// console.log(y = a - + d);
+// // console.log(y = a - e); - doesn`t work
+// // console.log(y = a - + e);  - doesn`t work
+// // console.log(y = a - f); - doesn`t work
+// console.log(y = a - g);
+// console.log(y = a - + g);
+
+// console.log(y = + b - a);
+// console.log(y = + b - + a);
+// console.log(y = b - c);
+// console.log(y = b - + c);
+// console.log(y = b - d);
+// console.log(y = b - + d);
+// // console.log(y = b - e);- doesn`t work
+// // console.log(y = b - + e); - doesn`t work
+// // console.log(y = b - + f);  - doesn`t work
+// console.log(y = b - g);
+// console.log(y = b - + g);
+
+// ================================ TAsk6 ==============================
