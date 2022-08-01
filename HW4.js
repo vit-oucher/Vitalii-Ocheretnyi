125fc7f1a329f8869d4dc68cc6952c24228f4c9e
vit-oucher <vitaliy.oucher.2@gmail.com>
2022-08-01 18:19:52 +0300

    task 1-4

---
+++ HW4.js
@@ -0,0 +1,51 @@
+// ======================================= Task1 ==============================
+// let array1 = ['a', 'b', 'c'];
+// let array2 = [1, 2, 3];
+//
+// console.log(array1);
+// console.log(array2);
+// const result1 = [array1, array2];
+// const result2 = array1.concat(array2);
+// const result3 = [].concat(array1, array2);
+//
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+// ======================================= Task2 ==================================
+
+// let array1 = ['a', 'b', 'c'];
+// console.log(array1);
+//
+// const result1 = array1.push(1, 2, 3);
+//
+// console.log(array1);
+// console.log(result1);
+
+// const result2 = array1.splice(1, 2, 3);
+// console.log(result2);
+// console.log(array1);
+
+// const result3 = array1.splice(3, 0, 1, 2, 3);
+// console.log(result3);
+// console.log(array1);
+
+// ======================================= Task3 ==================================
+
+// let arrNum = [1, 2, 3, 4, 5];
+// const result1 = arrNum.splice(1,2);
+// console.log(arrNum);
+// console.log(result1);
+
+// ======================================= Task4 ==================================
+
+// let arrNum = [1, 2, 3, 4, 5];
+// const res1 = arrNum.splice(2,0, 'a', 'b', 'c');
+// console.log(arrNum);
+// console.log(res1);
+
+// ======================================= Task5 ==================================
+
+let arr = [1, 2, 3, 4, 5];
+let arr1 = [];
+
