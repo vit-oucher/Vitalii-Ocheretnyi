768c23d8dd04884cde938a8eb9290b86971f47eb
vit-oucher <vitaliy.oucher.2@gmail.com>
2022-08-11 18:06:27 +0300

    hw6 in progress

--- HW6.js
+++ HW6.js
@@ -1,4 +1,40 @@
 // ========================== 1.1 ========================================
+// let start = '';
+// let end = '';
+// let salaryPerMonth = 0;
+// let position = '';
+// let companyName = '';
+let work = [
+    {
+        start: new Date ('2019-3-11'),
+        end: new Date ('2019-11-6'),
+        salaryPerMonth: 800,
+        position: 'junior',
+        companyName: 'Oracle',
+    },
+    {
+        start: new Date ('2019-11-06'),
+        end: new Date ('2020-01-07'),
+        salaryPerMonth: 1300,
+        position: 'middle',
+        companyName: 'ttk',
+    },
+    {
+        start: new Date ('2020-01-07'),
+        end: new Date ('2020-9-06'),
+        salaryPerMonth: 1500,
+        position: 'middle',
+        companyName: 'lpg',
+    },
+    {
+        start: new Date ('2020-9-06'),
+        end: new Date ('2021-11-10'),
+        salaryPerMonth: 2384,
+        position: 'middle',
+        companyName: 'trd',
+    }
+];
+
 class Human {
     constructor(height, weight, name, dateOfBirth, gender, disablement) {
         this.height = height;
@@ -49,54 +85,123 @@ get getInfo () {
     console.log('Info about human ', info);
 }
 }
-let firstHuman = new Human(185,100,'Vit', new Date("1996-2-25"),'male',false);
-console.log(firstHuman);
-firstHuman.getHello;
-firstHuman.setNewName = 'taras';
-firstHuman.setDisablementStatus = true;
-firstHuman.getInfo;
+// let firstHuman = new Human(185,100,'Vit', new Date("1996-2-25"),'male',false);
+// console.log(firstHuman);
+// firstHuman.getHello;
+// firstHuman.setNewName = 'taras';
+// firstHuman.setDisablementStatus = true;
+// firstHuman.getInfo;
 
 // ========================== 1.2 /// 1.3 ========================================
 
 class FrontEndDev extends Human {
-    constructor(startCarr, pastCompanies) {
-        super();
+    constructor(height, weight, name, dateOfBirth, gender, disablement, startCarr, pastCompanies) {
+        super(height, weight, name, dateOfBirth, gender, disablement);
         this.startCarr = startCarr;
         this.pastCompanies = pastCompanies;
-
-    }
-
+    };
 
 get getHelloFrEnd() {
     console.log('Привет, меня зовут ' + this.name + ', я Фронтенд разработчик. Работаю с ' + this.startCarr);
 
+};
+set totalSel (k) {
+        let money = [];
+        for (let i=0; i < work.length; i++) {
+            let x = work[i].start;
+            let y = work[i].end;
+            let monthDiff = (y.getFullYear() - x.getFullYear()) * 12;
+            monthDiff -= x.getMonth() + 1;
+            monthDiff += y.getMonth();
+
+            let total = monthDiff * work[i].salaryPerMonth;
+            money.push(total);
+        };
+            let l = 0
+            for (let j=0; j< money.length; j++){
+                l += money[j];
+            }
+            console.log(l);
+
+        };
+    getCompanyName(companyNameFind) {
+
+        for (let i=0; i<work.length; i++) {
+            if (work[i].companyName === companyNameFind)
+                console.log(work[i]);
+        };
+            };
+
+    newCompany (start, end, salaryPerMonth, position, companyName) {
+        let comp2 = {};
+        comp2 = {
+            start: start,
+            end: end,
+            salaryPerMonth: salaryPerMonth,
+            position: position,
+            companyName: companyName,
+        };
+        work.push(comp2);
+        console.log(work)
+
 }};
 
-let firsFrEDev = new FrontEndDev( new  Date ('2019-3-11'),{
-    start: new Date ('2019-3-11'),
-    end: new Date ('2021-11-6'),
-    salaryPerMonth: 1300,
-    position: 'middle',
-    companyName: 'Oracle',
-});
 
-console.log(firsFrEDev);
-firsFrEDev.setNewName = 'Iryna';
-firsFrEDev.setDisablementStatus = false;
-firsFrEDev.setDofBirth = '1995-6-23';
-firsFrEDev.setParamH = 180;
-firsFrEDev.setParamG = 'female';
-firsFrEDev.setParamW = 70;
+let secondHuman = new FrontEndDev (196, 95, 'Ihor', new Date('1998-06-05'), 'male',false, work[0].start, work[3].companyName);
+// secondHuman.totalSel = work;
+
+// console.log(secondHuman);
+
+// secondHuman.getHelloFrEnd;
+
+// secondHuman.newCompany(new Date('2021-11-23'), new Date('2022-03-25'), 964, 'senior', 'trd6');
+// secondHuman.getCompanyName('trd6');
 
 
 
 
+class HouseBuilder extends (Human) {
+    constructor(height, weight, name, dateOfBirth, gender, disablement, location, instruments, workSpeed) {
+        super(height, weight, name, dateOfBirth, gender, disablement);
+        this.location = location;
+        this.instruments = instruments;
+        this.workSpeed = workSpeed
+    };
+
+    building (kvm) {
+        let time = (kvm * this.workSpeed);
+        console.log(time);
+        if (time < 720) {
+            let hours = + Math.trunc(time / 60);
+            let min = time % 60;
+
+            console.log(hours + ' ч. и ' + min + ' мин.');
+        }
+        else if (time >= 720 && time < 5040) {
+            let days = + Math.trunc(time / 24)
+            let hours = + Math.trunc(days / 60);
+            let min = time % 60;
+
+            console.log(days + 'д.'+ hours +'ч. и ' + min + ' мин.');
+        }
+    };
+
+
+    // var sec = 262993;
+    // var min = sec / 60;
+    // var hour = min / 60;
+    // var day =
+    // 'Дней: ' + Math.floor(hour / 24) +
+    // ', часов: ' + Math.floor(hour % 24) +
+    // ', минут: ' + Math.floor(min % 60) +
+    // ', секунд: ' + Math.floor(sec % 60);
+    // alert(day);
 
 
 
+};
 
-// class HouseBuilder extends (Human) {
-//     constructor() {
-//         super();
-//     }
-// }
+let newHouseBuilder = new HouseBuilder(188,95, 'Rob', new Date('1985-12-05'), 'male', false, 'Kyiv', ['кирка', 'топор', 'кельма', 'руки)'],30);
+// console.log(newHouseBuilder);
+
+newHouseBuilder.building(50);
